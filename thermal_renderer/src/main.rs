442965ae9698ad90710707793ca508bc87a71315
yangdong <yangdong@shouqianba.com>
2023-03-15 11:56:36 +0800

    整合tesseract

--- thermal_renderer/src/main.rs
+++ thermal_renderer/src/main.rs
@@ -1,20 +1,28 @@
-use thermal_parser::{context::Context, command::Command};
+use thermal_parser::{context::Context,command::Command};
 use thermal_renderer::{image_renderer::ImageRenderer, renderer::CommandRenderer};
+use std::env;
+use std::process::Command as OsCommand;
 
 fn main(){
 
+    // 读取命令行参数
+    let args: Vec<String> = env::args().collect();
+    let bytes = std::fs::read(args[1].as_str()).unwrap();
 
-    let bytes = std::fs::read("ori.bin").unwrap();
     let vec_bytes:Vec<u8> = bytes.to_vec();
     let mut context = Context::new();
     let mut image_renderer = ImageRenderer::new(format!("{}/{}/{}", env!("CARGO_MANIFEST_DIR"), "resources", "out"));
-    println!("{:?}",image_renderer.out_path);
+    
     let on_new_command = move |cmd: Command| {
         image_renderer.process_command(&mut context, &cmd);
     };
 
     let mut command_parser = thermal_parser::new_esc_pos_parser(Box::from(on_new_command));
     command_parser.parse_bytes(&vec_bytes);
+
+    //let _tesseract_convert = OsCommand::new("tesseract").arg("ori.bin").arg("output").arg("-l").arg("chi_sim").output().expect("failed to execute process");
+    
+        
 
 }
 
